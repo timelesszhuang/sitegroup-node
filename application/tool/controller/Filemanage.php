48856d2fd23250bdc945c7c822cfec16976dd844
timelessz <834916321@qq.com>
2017-05-20 10:53:03 +0800

    # 模板文件上传完成

--- application/tool/controller/Filemanage.php
+++ application/tool/controller/Filemanage.php
@@ -11,6 +11,7 @@ namespace app\tool\controller;
 
 
 use app\common\controller\Common;
+use think\Exception;
 
 
 class Filemanage extends Common
@@ -22,12 +23,15 @@ class Filemanage extends Common
     static $zipActivityPath = 'upload/zipactivity';
 
     //html模板文件要解压到的地方
-    static $templateHtmlPath = 'template/';
+    static $templateHtmlPath = 'template';
     //静态模板文件解压到的地方
-    static $templateStaticPath = 'static/';
+    static $templateStaticPath = 'templatestatic';
     //活动要解压到的地方
-    static $activityPath = '/activity';
+    static $activityPath = 'activity';
+    //因为需要递归操作文件
+    static $arr_files = [];
 
+    //http://local.sitegroup.com/index.php/Site/uploadTemplateFile
 
     /**
      * 文件上传程序　
@@ -50,6 +54,33 @@ class Filemanage extends Common
     /**
      * 处理模板文件接收 以及解压缩操作
      * @access private
+     * 说明下模板文件的命名以及上传的机制
+     *
+     * 模板文件命名：
+     * 1 首先模板跟文件的命名为 /template/  下边 模板文件   ××.html
+     *                                                 ××1.html
+     *                            静态js css 图片文件   /templatestatic/ ××.css
+     *                                                                 ××.js
+     *                                                                 ××.img
+     * 解压缩机制
+     *
+     * 1、节点服务器后台 触发模板同步 post 文件到 uploadFile ,file 为 文件 ，type：template 表示模板 activity 为 活动
+     *
+     * 2、模板 首先解压缩在 public/template下  ，以为解压缩之后会有 目录 所以解压缩之后目录为  /public/template/template/
+     *
+     * 3、把/public/template/template/ 下的文件以及文件夹 都复制到  /public/template/ 目录下
+     *
+     * 4、然后复制 /public/template/templatestatic/下的js css img等文件到/public/templatestatic/下
+     *
+     * 5、 删除/public/template/templatestatic/ 目录 ，删除/public/template/template目录。
+     *
+     * 页面静态化机制
+     *
+     * 模板文件的位置为 /public/template ,设置tp 文件的视图文件位置为 该目录。
+     * 生成的静态文件放置在 /public 下,设置 index.html的优先级大于 index.php 设置 robots.txt 文件 不允许访问 router.php 跟 indx.php
+     *
+     * @todo 解压模板html文件到 /template 下 而不是 /template/文件名/××.html
+     *       解压 静态文件到/templatestatic 下 而不是 /template/文件名/css 等信息
      */
     private function manageTemplate()
     {
@@ -64,11 +95,25 @@ class Filemanage extends Common
 
         $status = '文件解压缩失败';
         //解压缩主题文件到指定的目录中
-        $realTemplatePath = $zipTemplateFilePath . '/' . $file_savename;
+        $realTemplatePath = $zipTemplateFilePath . DIRECTORY_SEPARATOR . $file_savename;
         $realTemplateUnzipPath = ROOT_PATH . 'public/' . self::$templateHtmlPath;
+        $realStaticPath = ROOT_PATH . 'public/' . self::$templateStaticPath;
         if ($this->unzipFile($realTemplatePath, $realTemplateUnzipPath)) {
-            //然后修改权限
+            //因为 模板文件解压缩之后 会在/template 下 生成文件夹  所以需要把 文件夹中的 文件复制到上级目录中
+            //复制 所有文件到上级目录中  然后删除 临时的模板解压文件
+            self::copydir_recurse($realTemplateUnzipPath . DIRECTORY_SEPARATOR . 'template', $realTemplateUnzipPath, true);
+            if (self::$arr_files) {
+                self::copy_only_file($realTemplateUnzipPath);
+            }
+            self::$arr_files = [];
             //解压的/public/template/static中的 静态文件 复制到/public/static 文件夹中
+            self::copydir_recurse($realTemplateUnzipPath . DIRECTORY_SEPARATOR . self::$templateStaticPath, $realStaticPath, true);
+            if (self::$arr_files) {
+                self::copy_only_file($realStaticPath);
+            }
+            //删除临时解压到的目录
+            self::deldirs($realTemplateUnzipPath . DIRECTORY_SEPARATOR . 'template');
+            self::deldirs($realTemplateUnzipPath . DIRECTORY_SEPARATOR . self::$templateStaticPath);
             $status = '文件解压缩成功';
         }
         if ($info) {
@@ -94,32 +139,32 @@ class Filemanage extends Common
      * @param $source 源目录名
      * @param $destination 目的目录名
      * @param $child 复制时，是不是包含的子目录
-     * @return array
      */
-    private static function copydir_recurse($source, $destination, $child)
+    private static function copydir_recurse($source, $destination, $child, $directory = '')
     {
-        $arr_file = [];
         if (!is_dir($source)) {
             echo("Error:the $source is not a direction!");
             return 0;
         }
         if (!is_dir($destination)) {
-            mkdir($destination, 0777);
+            mkdir($destination, 0777, true);
         }
         $handle = dir($source);
         while ($entry = $handle->read()) {
             if (($entry !== ".") && ($entry !== "..")) {
-                if (is_dir($source . "/" . $entry)) {
+                echo $source . '/' . $entry . '<br>';
+                if (is_dir($source . DIRECTORY_SEPARATOR . $entry)) {
                     if ($child)
-                        self::copydir_recurse($source . "/" . $entry, $destination .
-                            "/" . $entry, $child);
-                    self::deldirs($destination . "/" . $entry); //删除空文件夹
+                        self::copydir_recurse($source . DIRECTORY_SEPARATOR . $entry, $destination . DIRECTORY_SEPARATOR . $entry, $child, $entry);
                 } else {
-                    $arr_file[$source . "/" . $entry] = $entry;
+                    $file_name = $entry;
+                    if ($directory) {
+                        $file_name = $directory . DIRECTORY_SEPARATOR . $entry;
+                    }
+                    self::$arr_files[$source . DIRECTORY_SEPARATOR . $entry] = $file_name;
                 }
             }
         }
-        return $arr_file;
     }
 
 
@@ -132,15 +177,17 @@ class Filemanage extends Common
      * $arr_file这个数组是从 copydir_recurse中得到的;
      * $file_arr:文件数组，键名为包含原文件夹下的所有文件路经，键值为目录文件名;
      * $aim_dir:目标目录名
-     * @param $arr_file
      * @param $aim_dir
      */
-    private static function copy_only_file($arr_file, $aim_dir)
+    private static function copy_only_file($aim_dir)
     {
-        foreach (self::$arr_file as $key => $value) {
-            if (is_dir($value))
-                self::deldirs($value);//删除空目录
-            copy($key, $aim_dir . '\ ' . $value);
+//        echo $aim_dir;
+        foreach (self::$arr_files as $key => $value) {
+            try {
+                copy($key, $aim_dir . DIRECTORY_SEPARATOR . $value);
+            } catch (Exception $ex) {
+//                print_r($ex);
+            }
         }
     }
 
