c18a5fec194af2aa1a683e589562a176223c5425
timelessz <834916321@qq.com>
2017-06-05 15:50:26 +0800

    # 目录结构划分完成

--- application/tool/controller/Indexstatic.php
+++ application/tool/controller/Indexstatic.php
@@ -83,7 +83,7 @@ class Indexstatic extends Common
         $after_head = $siteinfo['other_jscode'];
         //公司名称
         $com_name = $siteinfo['com_name'];
-        $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'before_head', 'after_head', 'chain_type', 'next_site','main_site','common_site','partnersite','commonjscode','article_list','question_list','scatteredarticle_list');
+        $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'menu', 'before_head', 'after_head', 'chain_type', 'next_site', 'main_site', 'common_site', 'partnersite', 'commonjscode', 'article_list', 'question_list', 'scatteredarticle_list');
         file_put_contents('log/index.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
         //页面中还需要填写隐藏的 表单 node_id site_id
         $content = (new View())->fetch('template/index.html',
