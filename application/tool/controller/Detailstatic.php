801082004fdb7e79e4328d91a11300defb067473
yangjingzheng <1318911846@qq.com>
2017-06-07 17:35:05 +0800

    local

--- application/tool/controller/Detailstatic.php
+++ application/tool/controller/Detailstatic.php
@@ -5,6 +5,7 @@ namespace app\tool\controller;
 use app\common\controller\Common;
 use app\index\model\ArticleSyncCount;
 use app\index\model\Articletype;
+use app\index\model\ScatteredArticle;
 use app\index\model\ScatteredTitle;
 use think\Db;
 use think\View;
@@ -109,11 +110,11 @@ class Detailstatic extends Common
                 $articleCountModel = ArticleSyncCount::where($where)->find();
                 if (is_null($articleCountModel)) {
                     $article_temp->count = $item->id;
-                    $article_temp->type_id =$type_id;
+                    $article_temp->type_id = $type_id;
                     $article_temp->type_name = $type_name;
                     $article_temp->node_id = $node_id;
                     $article_temp->site_id = $site_id;
-                    $article_temp->site_name=$site_name;
+                    $article_temp->site_name = $site_name;
                     $article_temp->save();
                 } else {
                     $articleCountModel->count = $item->id;
@@ -130,53 +131,73 @@ class Detailstatic extends Common
      * @param $type_id 文章的分类id
      * @param $a_keyword_id 栏目所对应的a类 关键词
      */
-    public function scatteredarticle($site_id, $site_name, $node_id, $type_id = 5, $a_keyword_id)
+    public function scatteredarticlestatic($site_id, $site_name, $node_id, $type_id, $a_keyword_id)
+
     {
         //  获取详情 页生成需要的资源  首先需要比对下当前页面是不是已经静态化了
         //  关键词
         $type_name = "scatteredarticle";
-
         $where = [
-            'articletype_id' => $type_id,
+            'type_id' => $type_id,
             'type_name' => $type_name,
             "node_id" => $node_id,
             "site_id" => $site_id
         ];
         $limit = 0;
         $articleCount = ArticleSyncCount::where($where)->find();
-        if (isset($articleCount->count) && ($articleCount->count) > 0) {
+        //判断下是否有数据 没有就创建模型
+        if (isset($articleCount->count) && $articleCount->count > 0) {
             $limit = $articleCount->count;
+        } else {
+            $article_temp = new ArticleSyncCount();
         }
-        $count = \app\index\model\Article::where(["articletype_id" => $type_id, "node_id" => $node_id])->count();
-        $article_data = \app\index\model\Article::where(["articletype_id" => $type_id, "node_id" => $node_id])->order("id", "asc")->limit($limit, $count)->select();
-        foreach ($article_data as $item) {
-            $temp_content = mb_substr(strip_tags($item->content), 0, 200);
-            list($com_name, $title, $keyword, $description,
-                $m_url, $redirect_code, $menu, $before_head,
-                $after_head, $chain_type, $next_site,
-                $main_site, $partnersite, $commonjscode,
-                $article_list, $question_list, $scatteredarticle_list) = Commontool::getEssentialElement('detail', $item->title, $temp_content, $a_keyword_id);
-            $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'menu', 'before_head', 'after_head', 'chain_type', 'next_site', 'main_site', 'common_site', 'partnersite', 'commonjscode', 'article_list', 'question_list', 'scatteredarticle_list');
+        $scatTitleArray = (new ScatteredTitle())->where(["articletype_id" => $type_id])->select();
+        foreach ($scatTitleArray as $item) {
+            $scatArticleArray = ScatteredArticle::where(["id" => ["in", $item->article_ids]])->select();
+//            $temp_content = $scatArticleArray->content_paragraph;
+            foreach ($scatArticleArray as $value) {
+                $content = $item["title"] . "</br>" . $value['content_paragraph'];
+                $temp_content = mb_substr(strip_tags($content), 0, 200);
+                list($com_name, $title, $keyword, $description,
+                    $m_url, $redirect_code, $menu, $before_head,
+                    $after_head, $chain_type, $next_site,
+                    $main_site, $partnersite, $commonjscode,
+                    $article_list, $question_list, $scatteredarticle_list) = Commontool::getEssentialElement('detail', $item->title, $temp_content, $a_keyword_id);
+                $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'menu', 'before_head', 'after_head', 'chain_type', 'next_site', 'main_site', 'common_site', 'partnersite', 'commonjscode', 'article_list', 'question_list', 'scatteredarticle_list');
 //                    file_put_contents('log/article.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
-            //页面中还需要填写隐藏的 表单 node_id site_id
-            $content = (new View())->fetch('template/article_make.html',
-                [
-                    'd' => $assign_data,
-                    'article' => $item,
+                //页面中还需要填写隐藏的 表单 node_id site_id
+                //获取上一篇和下一篇
+                $pre_article = \app\index\model\ScatteredArticle::where(["id" => ["lt", $value["id"]]])->find();
+                $next_article = \app\index\model\ScatteredArticle::where(["id" => ["gt", $value["id"]]])->find();
+                $content = (new View())->fetch('template/newslist.html',
+                    [
+                        'd' => $assign_data,
+                        'article' => $value,
+                        'pre_article' => $pre_article,
+                        'next_article' => $next_article
+                    ]
+                );
+                $make_web = file_put_contents('newslist/newslist' . $value["id"] . '.html', $content);
+                //开始同步数据库
+                if ($make_web) {
+                    $articleCountModel = ArticleSyncCount::where($where)->find();
+                    if (is_null($articleCountModel)) {
+                        $article_temp->count = $value->id;
+                        $article_temp->articletype_id = $type_id;
+                        $article_temp->articletype_name = $type_name;
+                        $article_temp->node_id = $node_id;
+                        $article_temp->site_id = $site_id;
+                        $article_temp->site_name = $site_name;
+                        $article_temp->save();
+                    } else {
+                        $articleCountModel->count = $value->id;
+                        $articleCountModel->save();
+                    }
+                }
 
-                ]
-            );
-            file_put_contents('article/article' . $item["id"] . '.html', $content);
+
+            }
         }
-        $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'menu', 'before_head', 'after_head', 'chain_type', 'next_site', 'main_site', 'common_site', 'partnersite', 'commonjscode', 'article_list', 'question_list', 'scatteredarticle_list');
-        file_put_contents('log/article.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
-        //页面中还需要填写隐藏的 表单 node_id site_id
-        $content = (new View())->fetch('template/newslist.html',
-            [
-                'd' => $assign_data
-            ]
-        );
-        file_put_contents('index.html', $content);
 
 
     }
@@ -230,11 +251,11 @@ class Detailstatic extends Common
                 $articleCountModel = ArticleSyncCount::where($where)->find();
                 if (is_null($articleCountModel)) {
                     $article_temp->count = $item->id;
-                    $article_temp->type_id =$type_id;
+                    $article_temp->type_id = $type_id;
                     $article_temp->type_name = $type_name;
                     $article_temp->node_id = $node_id;
                     $article_temp->site_id = $site_id;
-                    $article_temp->site_name=$site_name;
+                    $article_temp->site_name = $site_name;
                     $article_temp->save();
                 } else {
                     $articleCountModel->count = $item->id;
