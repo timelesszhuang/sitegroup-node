5dd688c9c7741ea40109efc5a94c044ec2718619
yangjingzheng <1318911846@qq.com>
2017-06-07 10:34:06 +0800

    Merge branch 'dev' of https://github.com/timelesszhuang/sitegroup-node into dev
    # Conflicts:
    #	application/index/controller/ArticleList.php

--- application/tool/controller/Detailstatic.php
+++ application/tool/controller/Detailstatic.php
@@ -71,8 +71,40 @@ class Detailstatic extends Common
             ]
         );
         file_put_contents('index.html', $content);
-
-
     }
+
+
+
+
+    /**
+     * 零散文章的静态化
+     * @access public
+     * @todo 需要比对 哪个已经生成静态页面了  哪个没有生成静态页面
+     * @param $type_id 文章的分类id
+     * @param $a_keyword_id 栏目所对应的a类 关键词
+     */
+    public function scatteredarticle($type_id, $a_keyword_id)
+    {
+        //  获取详情 页生成需要的资源  首先需要比对下当前页面是不是已经静态化了
+        //  关键词
+
+        list($com_name, $title, $keyword, $description,
+            $m_url, $redirect_code, $menu, $before_head,
+            $after_head, $chain_type, $next_site,
+            $main_site, $partnersite, $commonjscode,
+            $article_list, $question_list, $scatteredarticle_list) = Commontool::getEssentialElement('detail',$title,$content,$a_keyword_id);
+
+        $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'menu', 'before_head', 'after_head', 'chain_type', 'next_site', 'main_site', 'common_site', 'partnersite', 'commonjscode', 'article_list', 'question_list', 'scatteredarticle_list');
+        file_put_contents('log/article.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
+        //页面中还需要填写隐藏的 表单 node_id site_id
+        $content = (new View())->fetch('template/newslist.html',
+            [
+                'd' => $assign_data
+            ]
+        );
+        file_put_contents('index.html', $content);
+    }
+
+
 
 }
