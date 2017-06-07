f378f3d0449c50699acc3efa9907672532ebc53c
2301887641 <sgz19861126>
2017-06-07 17:31:32 +0800

    Merge branch 'dev' of https://github.com/timelesszhuang/sitegroup-node into dev
    # Conflicts:
    #	application/tool/controller/Detailstatic.php

--- application/tool/controller/Detailstatic.php
+++ application/tool/controller/Detailstatic.php
@@ -5,6 +5,7 @@ namespace app\tool\controller;
 use app\common\controller\Common;
 use app\index\model\ArticleSyncCount;
 use app\index\model\Articletype;
+use app\index\model\Question;
 use app\index\model\ScatteredTitle;
 use think\Db;
 use think\View;
@@ -38,15 +39,15 @@ class Detailstatic extends Common
                 }
                 $a_keyword_id = $menu_akeyword_id_arr[$type['menu_id']];
                 switch ($detail_key) {
-                    case'article':
-                        $this->articlestatic($site_id, $site_name, $node_id, $type['id'], $menu_akeyword_id_arr[$type['menu_id']]);
-                        break;
+//                    case'article':
+//                        $this->articlestatic($site_id, $site_name, $node_id, $type['id'], $a_keyword_id);
+//                        break;
                     case'question':
-                        $this->questionstatic($type['id'], $menu_akeyword_id_arr[$type['menu_id']]);
+                        $this->questionstatic($site_id, $site_name, $node_id,$type['id'], $a_keyword_id);
                         break;
-                    case'scatteredarticle':
-                        $this->scatteredarticlestatic($site_id, $site_name, $node_id,$type['id'], $menu_akeyword_id_arr[$type['menu_id']]);
-
+//                    case'scatteredarticle':
+//                        $this->scatteredarticlestatic($site_id, $site_name, $node_id,$type['id'], $a_keyword_id);
+//                        break;
                 }
             }
         }
@@ -181,12 +182,20 @@ class Detailstatic extends Common
 
     }
 
+    /**
+     * 问答
+     * @param $site_id
+     * @param $site_name
+     * @param $node_id
+     * @param $type_id
+     * @param $a_keyword_id
+     */
     public function questionstatic($site_id, $site_name, $node_id, $type_id, $a_keyword_id)
     {
         //  获取详情 页生成需要的资源  首先需要比对下当前页面是不是已经静态化了
         //  关键词
         //当前分类名称
-        $type_name = "article";
+        $type_name = "question";
         $where = [
             'type_id' => $type_id,
             'type_name' => $type_name,
@@ -201,30 +210,30 @@ class Detailstatic extends Common
         } else {
             $article_temp = new ArticleSyncCount();
         }
-        $count = \app\index\model\Article::where(["articletype_id" => $type_id, "node_id" => $node_id])->count();
-        $article_data = \app\index\model\Article::where(["articletype_id" => $type_id, "node_id" => $node_id])->order("id", "asc")->limit($limit, $count)->select();
-        foreach ($article_data as $item) {
-            $temp_content = mb_substr(strip_tags($item->content), 0, 200);
+        $count = \app\index\model\Question::where(["type_id" => $type_id, "node_id" => $node_id])->count();
+        $question_data = \app\index\model\Question::where(["type_id" => $type_id, "node_id" => $node_id])->order("id", "asc")->limit($limit, $count)->select();
+        foreach ($question_data as $item) {
+            $temp_content = mb_substr(strip_tags($item->content_paragraph), 0, 200);
             list($com_name, $title, $keyword, $description,
                 $m_url, $redirect_code, $menu, $before_head,
                 $after_head, $chain_type, $next_site,
                 $main_site, $partnersite, $commonjscode,
-                $article_list, $question_list, $scatteredarticle_list) = Commontool::getEssentialElement('detail', $item->title, $temp_content, $a_keyword_id);
+                $article_list, $question_list, $scatteredarticle_list) = Commontool::getEssentialElement('detail', $item->question, $temp_content, $a_keyword_id);
             $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'menu', 'before_head', 'after_head', 'chain_type', 'next_site', 'main_site', 'common_site', 'partnersite', 'commonjscode', 'article_list', 'question_list', 'scatteredarticle_list');
 //                    file_put_contents('log/article.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
             //页面中还需要填写隐藏的 表单 node_id site_id
             //获取上一篇和下一篇
-            $pre_article = \app\index\model\Article::where(["id" => ["lt", $item["id"]]])->find();
-            $next_article = \app\index\model\Article::where(["id" => ["gt", $item["id"]]])->find();
-            $content = (new View())->fetch('template/article_make.html',
+            $pre_article = \app\index\model\Question::where(["id" => ["lt", $item["id"]]])->find();
+            $next_article = \app\index\model\Question::where(["id" => ["gt", $item["id"]]])->find();
+            $content = (new View())->fetch('template/question_make.html',
                 [
                     'd' => $assign_data,
-                    'article' => $item,
+                    'question' => $item,
                     'pre_article' => $pre_article,
                     'next_article' => $next_article
                 ]
             );
-            $make_web = file_put_contents('article/article' . $item["id"] . '.html', $content);
+            $make_web = file_put_contents('article/question' . $item["id"] . '.html', $content);
             //开始同步数据库
             if ($make_web) {
                 $articleCountModel = ArticleSyncCount::where($where)->find();
