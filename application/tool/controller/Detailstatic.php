6c2db4b30f80e4c5de6aec95242c234408272954
2301887641 <sgz19861126>
2017-06-07 14:32:04 +0800

    local

--- application/tool/controller/Detailstatic.php
+++ application/tool/controller/Detailstatic.php
@@ -81,11 +81,17 @@ class Detailstatic extends Common
                         $article_list, $question_list, $scatteredarticle_list) = Commontool::getEssentialElement('detail',$item->title,$temp_content,$a_keyword_id);
                     $assign_data = compact('com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'menu', 'before_head', 'after_head', 'chain_type', 'next_site', 'main_site', 'common_site', 'partnersite', 'commonjscode', 'article_list', 'question_list', 'scatteredarticle_list');
 //                    file_put_contents('log/article.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
+
+                    //获取上一篇和下一篇
+                    $pre_article=\app\index\model\Article::where(["id"=>["lt",$item["id"]]])->find();
+                    $next_article=\app\index\model\Article::where(["id"=>["gt",$item["id"]]])->find();
                     //页面中还需要填写隐藏的 表单 node_id site_id
                     $content = (new View())->fetch('template/article_make.html',
                         [
                             'd' => $assign_data,
-                            'article'=>$item
+                            'article'=>$item,
+                            'pre_article'=>$pre_article,
+                            'next_article'=>$next_article
                         ]
                     );
                     file_put_contents('article/article'.$item["id"].'.html', $content);
