c3540ebf7dabf52f35ecd245404d9a66383a9cbc
2301887641 <sgz19861126>
2017-06-08 16:17:47 +0800

    Merge branch 'dev' of https://github.com/timelesszhuang/sitegroup-node into dev
    # Conflicts:
    #	application/tool/controller/Detailstatic.php

--- application/index/controller/QuestionList.php
+++ application/index/controller/QuestionList.php
@@ -9,19 +9,24 @@ use app\tool\controller\Keyword;
 use app\tool\controller\Menu;
 use app\tool\controller\Site;
 use think\View;
-
+use app\tool\controller\FileExistsTraits;
 /**
  * 文章列表相关操作 列表伪静态
  * 栏目下的文章 相关操作
  */
 class QuestionList extends Common
 {
+    use FileExistsTraits;
     /**
      * 首页列表
      * @access public
      */
     public function index($id)
     {
+        //判断模板是否存在
+        if(!$this->fileExits('template/questionlist.html')){
+            return;
+        }
         $siteinfo = Site::getSiteInfo();
         if(empty($siteinfo["menu"])){
             exit("当前栏目为空");
@@ -41,7 +46,7 @@ class QuestionList extends Common
         $assign_data = compact('question','com_name', 'title', 'keyword', 'description', 'm_url', 'redirect_code', 'before_head', 'after_head', 'chain_type', 'next_site','main_site','common_site','partnersite','commonjscode','article_list','question_list','scatteredarticle_list');
         //file_put_contents('log/questionlist.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
         //页面中还需要填写隐藏的 表单 node_id site_id
-        return  (new View())->fetch('template/question.html',
+        return  (new View())->fetch('template/questionlist.html',
             [
                 'd' => $assign_data
             ]
