0c9734a236cb6b56737eb761c5df3b7a81adbdec
yangjingzheng <1318911846@qq.com>
2017-06-12 10:38:39 +0800

    Merge branch 'dev' of https://github.com/timelesszhuang/sitegroup-node into dev
    # Conflicts:
    #	application/tool/controller/Detailstatic.php

--- application/tool/controller/Commontool.php
+++ application/tool/controller/Commontool.php
@@ -20,6 +20,7 @@ class Commontool extends Common
      */
     public function clearCache()
     {
+        file_put_contents("111.txt","dddd");
         if (Cache::clear()) {
             exit(['status' => 'success', 'msg' => '清除缓存成功。']);
         }
