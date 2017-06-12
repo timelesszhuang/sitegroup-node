ed0ed99adc218244e14c81b9c2a811628e598ebc
2301887641 <sgz19861126>
2017-06-12 14:49:04 +0800

    closure

--- application/tool/controller/Detailstatic.php
+++ application/tool/controller/Detailstatic.php
@@ -48,9 +48,9 @@ class Detailstatic extends Common
                     case'article':
                         return $this->articlestatic($site_id, $site_name, $node_id, $type['id'], $a_keyword_id);
                         break;
-//                    case'question':
-//                        return $this->questionstatic($site_id, $site_name, $node_id, $type['id'], $a_keyword_id);
-//                        break;
+                    case'question':
+                        return $this->questionstatic($site_id, $site_name, $node_id, $type['id'], $a_keyword_id);
+                        break;
 //                    case'scatteredarticle':
 //                        return $this->scatteredarticlestatic($site_id, $site_name, $node_id, $type['id'], $menu_akeyword_id_arr[$type['menu_id']]);
 //                        break;
