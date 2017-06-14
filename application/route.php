f88cd5df478775aa2dbbd29236011d9795bf02e4
2301887641 <sgz19861126>
2017-06-14 14:40:05 +0800

    local

--- application/route.php
+++ application/route.php
@@ -33,3 +33,5 @@ Route::get('menustatic', 'tool/Pagestaticentry/menustatic');
 Route::get('articlestatic', 'tool/Pagestaticentry/articlestatic');
 //清除缓存 默认使用文件缓存
 Route::get('clearCache', 'tool/Commontool/clearCache');
+//生成sitemap
+Route::get('sitemap','tool/SiteMap/index');
