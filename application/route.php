0bfc2dc5cc1606437b745fb7c8b008d5ad68ccfc
timelessz <834916321@qq.com>
2017-06-28 14:35:14 +0800

    #

--- application/route.php
+++ application/route.php
@@ -24,14 +24,15 @@ Route::get('articlelist/:id', 'index/ArticleList/index', ['ext' => 'html']);
 //模板文件 活动文件管理
 Route::rule('filemanage/uploadFile', 'tool/Filemanage/uploadFile');
 
-//模板文件相关操作
+//模板文件列表相关操作
 Route::get('templatelist', 'tool/Template/templatelist');
-//读取文件
+//读取模板文件
 Route::get('templateread', 'tool/Template/templateread');
-//更新文件
+//更新模板文件
 Route::get('templateupdate', 'tool/Template/templateupdate');
 //添加新模板文件
 Route::get('templateadd', 'tool/Template/templateadd');
+
 
 
 //全部页面静态化
