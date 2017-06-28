163799375cd3561a8492d9d9a2e5b8babaa94540
2301887641 <2301887641@qq.com>
2017-06-28 14:30:00 +0800

    local

--- public/templatestatic/aaa.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){  
-    $(".meundh li a").each(function(){  
-        $this = $(this);  
-        if($this[0].href==String(window.location)){  
-            $this.addClass("selected");  
-        }  
-    });  
-});
