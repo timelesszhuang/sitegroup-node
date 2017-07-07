cdee90a18dd4d7af97ca40b1c78e0c2981a8f77d
2301887641 <2301887641@qq.com>
2017-07-07 11:51:08 +0800

    模板

--- application/index/controller/ArticleList.php
+++ application/index/controller/ArticleList.php
@@ -56,13 +56,11 @@ class ArticleList extends Common
         $assign_data = Commontool::getEssentialElement('menu', $menu_info->generate_name, $menu_info->name, $menu_info->id);
         //取出同步的总数
         $articleSyncCount = ArticleSyncCount::where(["site_id" => $data["site_id"], "node_id" => $data["node_id"], "type_name" => "article", 'type_id' => $menu_info['type_id']])->find();
-        $where["articletype_id"] = $menu_info->type_id;
         $article=[];
-
         if ($articleSyncCount) {
-            $where["id"] = ["elt", $articleSyncCount->count];
+            $where="id <={$articleSyncCount->count} and node_id={$siteinfo['node_id']} and articletype_id={$menu_info->type_id} and is_sync=20 or  (id <={$articleSyncCount->count} and node_id={$siteinfo['node_id']} and articletype_id={$menu_info->type_id} and site_id = {$siteinfo['id']})";
             //获取当前type_id的文章
-            $article = \app\index\model\Article::order('id', "desc")->where($where)->where(["is_sync"=>20])->whereOr(["site_id"=>$siteinfo['id']])->paginate(10);
+            $article = \app\index\model\Article::order('id', "desc")->where($where)->paginate(10);
         }
         $assign_data['article'] = $article;
         file_put_contents('log/questionlist.txt', $this->separator . date('Y-m-d H:i:s') . print_r($assign_data, true) . $this->separator, FILE_APPEND);
