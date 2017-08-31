8cc5c91e89665d300a9ba421782a049d010fe44a
2301887641 <2301887641@qq.com>
2017-08-31 11:47:44 +0800

    article中需要根据指定的关键词进行替换链接

--- application/tool/controller/FileExistsTraits.php
+++ application/tool/controller/FileExistsTraits.php
@@ -598,7 +598,7 @@ trait FileExistsTraits
         $content='';
         $filename=ROOT_PATH."public/".$type."/".$name.".html";
         if(file_exists($filename)){
-        $content=gzdeflate(file_get_contents($filename));
+        $content=bzcompress(file_get_contents($filename));
         return json_encode([
             "msg"=>"",
             "status"=>"success",
