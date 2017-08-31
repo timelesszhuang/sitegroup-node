ed59530e22d7ab065efbd3dcd68777f77fc7d98b
2301887641 <2301887641@qq.com>
2017-08-31 10:09:40 +0800

    article中需要根据指定的关键词进行替换链接

--- application/tool/controller/FileExistsTraits.php
+++ application/tool/controller/FileExistsTraits.php
@@ -599,7 +599,7 @@ trait FileExistsTraits
         $filename=ROOT_PATH."public/".$type."/".$name.".html";
         if(file_exists($filename)){
         $content=file_get_contents($filename);
-        die(print_r([
+        die(json_encode([
             "msg"=>"",
             "status"=>"success",
             "data"=>gzcompress($content,9)
