cd4bc7eef9403939caa6580a5124d70b7c7be3da
2301887641 <2301887641@qq.com>
2017-09-20 11:39:56 +0800

    事件营销活动

--- application/tool/controller/FileExistsTraits.php
+++ application/tool/controller/FileExistsTraits.php
@@ -673,6 +673,34 @@ trait FileExistsTraits
     }
 
     /**
+     * 发送curl post请求
+     * @param $url
+     * @param $data post数据 数组格式
+     * @return mixed
+     */
+    public function curl_post($url,$data)
+    {
+        //初始化
+        $curl = curl_init();
+        //设置抓取的url
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //设置头文件的信息作为数据流输出
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        //设置获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        //设置post方式提交
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        //执行命令
+        $data = curl_exec($curl);
+        //关闭URL请求
+        curl_close($curl);
+        //显示获得的数据
+        return $data;
+    }
+
+    /**
      * ping百度程序
      * @param $data
      */
@@ -699,7 +727,6 @@ ENF;
         </param>
 ONE;
             $html.="</params></methodCall>";
-            dump($html);die;
             $this->curl_post("http://ping.baidu.com/ping/RPC2",$html);
         }
     }
