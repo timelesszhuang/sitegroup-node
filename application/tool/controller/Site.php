39236623f4de235cecf62bc0ee1248a2d6d0bee2
yangjingzheng <1318911846@qq.com>
2017-11-10 17:41:58 +0800

    shuaidan

--- application/tool/controller/Site.php
+++ application/tool/controller/Site.php
@@ -331,7 +331,7 @@ class Site extends Common
         $rejectionfinish =  (new Rejection())->order('id desc')->find();
         if($rejectionfinish){
             if($rejectionfinish['field2'] ==  $data["field2"]){
-                return $this->resultArray("重复申请", "failed");
+                return $this->resultArray("请不要重复申请", "failed");
             }
         }
         //提交甩单次数过多
