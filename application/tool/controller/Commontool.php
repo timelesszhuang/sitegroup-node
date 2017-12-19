3b39b9bee54bb659bfca9347c1612a69ec7eb0cf
yangjingzheng <1318911846@qq.com>
2017-12-19 13:58:09 +0800

    break

--- application/tool/controller/Commontool.php
+++ application/tool/controller/Commontool.php
@@ -1461,7 +1461,9 @@ code;
         ];
         switch ($tag) {
             case 'index':
+                break;
             case 'menu':
+                break;
             case 'detail':
                 //详情 或者 列表页面
                 $menu_path = \app\tool\model\Menu::Where('id', $menu_id)->field('path')->find();
