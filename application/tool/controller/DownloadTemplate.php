3bcdfa17741d1a8530d147e7216d9e61ec5597f1
timelessz <834916321@qq.com>
2018-03-09 14:12:40 +0800

    # 下载文件操作

--- application/tool/controller/DownloadTemplate.php
+++ application/tool/controller/DownloadTemplate.php
@@ -27,14 +27,33 @@ class DownloadTemplate extends Common
         if (!$filetoken) {
             exit(json_encode(['status' => 'failed', '参数异常']));
         }
-        $filename = Coding::tiriDecode($filetoken);
-        $filename = ROOT_PATH . 'public' . $filename;
-        $file = fopen($filename, "r");
-        header("Content-Type: application/octet-stream");
-        header("Accept-Ranges: bytes");
-        header("Accept-Length: " . filesize($filename));
-        header("Content-Disposition: attachment; filename=a.jpg");
-        echo fread($file, filesize($filename));
-        fclose($file);
+        $filerelativename = Coding::tiriDecode($filetoken);
+        $filepath = ROOT_PATH . 'public' . $filerelativename;
+        $pathinfo = pathinfo($filepath);
+        $file_name = $pathinfo['basename'];
+        header("Content-type:text/html;charset=utf-8");
+        //用以解决中文不能显示出来的问题
+        $file_name = iconv("utf-8", "gb2312", $file_name);
+        //首先要判断给定的文件存在与否
+        if (!file_exists($filepath)) {
+            echo "没有该文件文件";
+            return;
+        }
+        $fp = fopen($filepath, "r");
+        $filesize = filesize($filepath);
+        //下载文件需要用到的头
+        Header("Content-type: application/octet-stream");
+        Header("Accept-Ranges: bytes");
+        Header("Accept-Length:" . $filesize);
+        Header("Content-Disposition: attachment; filename=" . $file_name);
+        $buffer = 1024;
+        $file_count = 0;
+        //向浏览器返回数据
+        while (!feof($fp) && $file_count < $filesize) {
+            $file_con = fread($fp, $buffer);
+            $file_count += $buffer;
+            echo $file_con;
+        }
+        fclose($fp);
     }
 }
