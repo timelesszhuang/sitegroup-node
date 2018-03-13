411be80ece777bc46e765080e12e239845b98f2f
timelessz <834916321@qq.com>
2018-03-13 13:53:40 +0800

    #  添加泛站解析

--- application/common/controller/EntryCommon.php
+++ application/common/controller/EntryCommon.php
@@ -1,9 +1,11 @@
 <?php
+
 // +----------------------------------------------------------------------
 // | Description: 进入页面之后继承的公共元素
 // +----------------------------------------------------------------------
 // | Author: timelesszhuang <834916321@qq.com>
 // +----------------------------------------------------------------------
+
 
 namespace app\common\controller;
 
@@ -12,7 +14,6 @@ use app\index\traits\Pv;
 use app\index\traits\SearchEngineComefrom;
 use app\index\traits\SpiderComefrom;
 use app\tool\controller\Site;
-use think\Config;
 
 class EntryCommon extends Common
 {
@@ -20,17 +21,30 @@ class EntryCommon extends Common
     use Pv;
     use SearchEngineComefrom;
 
+    public $siteinfo;
+
+    //入口的位置执行相关请求
+
+    public function __construct()
+    {
+        $this->siteinfo = Site::getSiteInfo();
+        parent::__construct();
+        //截取下相关的域名
+        $host = $_SERVER['HTTP_HOST'];
+        print_r($host);
+    }
+
+
     /**
      * 浏览页面之后的公共操作
      */
     public function entryCommon()
     {
-        $siteinfo = Site::getSiteInfo();
         $uri = $_SERVER['REQUEST_URI'];
-        $absolutepath = $siteinfo['url'] . $uri;
-        $this->spidercomefrom($siteinfo, $absolutepath);
-        $this->pv($siteinfo, $absolutepath);
+        $absolutepath = $this->siteinfo['url'] . $uri;
+        $this->spidercomefrom($this->siteinfo, $absolutepath);
+        $this->pv($this->siteinfo, $absolutepath);
         //获取请求的useragent
-        $this->pagecomefrom($siteinfo, $absolutepath);
+        $this->pagecomefrom($this->siteinfo, $absolutepath);
     }
 }
