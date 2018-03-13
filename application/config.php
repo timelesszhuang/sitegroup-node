27031cf29bee56a8a2ab82690db62cc855dce326
timelessz <834916321@qq.com>
2018-03-13 14:31:45 +0800

    #  修改首页文件名

--- application/config.php
+++ application/config.php
@@ -17,7 +17,7 @@ return [
     // 应用命名空间
     'app_namespace' => 'app',
     // 应用调试模式
-    'app_debug' => false,
+    'app_debug' => true,
     // 应用Trace
     'app_trace' => false,
     // 应用模式状态
@@ -60,7 +60,7 @@ return [
     // 禁止访问模块
     'deny_module_list' => ['common'],
     // 默认控制器名
-    'default_controller' => 'Index',
+    'default_controller' => 'Detailenter',
     // 默认操作名
     'default_action' => 'index',
     // 默认验证器
