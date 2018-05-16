55f644f0979252828f2f230cbce0f2afeac95819
jingyang <linshuboy@qq.com>
2018-05-16 14:42:19 +0800

    修复链接拼接错误的问题

--- application/tool/model/Childsitelist.php
+++ application/tool/model/Childsitelist.php
@@ -19,11 +19,11 @@ class Childsitelist extends Model
         return Cache::remember('childsitelist' . $site_id, function () use ($site_id) {
             $childsitelist = $this->where(['site_id' => $site_id])->field('district_id as id,en_name,name,p_id')->order(['sort' => 'desc', 'district_id' => 'asc'])->select();
             $site_info = Db::name('Site')->where(['id' => $site_id])->find();
-            if(!($site_info&&$childsitelist)){
+            if (!($site_info && $childsitelist)) {
                 return [];
             }
             foreach ($childsitelist as $key => $value) {
-                $childsitelist[$key]['url'] = 'http://' . $value['en_name'] . $site_info['domain'];
+                $childsitelist[$key]['url'] = 'http://' . $value['en_name'] . "." . $site_info['domain'];
             }
             return $childsitelist;
         });
